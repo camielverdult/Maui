e2cd720905f1d476cac760c426c84ad4a3d5116b
camielverdult <camiel@camiel.pw>
2021-06-18 11:01:14 +0200

    Add bindings

--- Maui/MainPage.xaml.cs
+++ Maui/MainPage.xaml.cs
@@ -27,64 +27,13 @@ namespace Maui
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
         }
 
         void NewSiteEntry(Object sender,
                            EventArgs e)
         {
 
-            var NewGrid = new Grid
-            {
-                WidthRequest = 400,
-                HeightRequest = 100
-            };
-            // NewGrid.HorizontalAlignment = HorizontalAlignment.Left;
-            // NewGrid.VerticalAlignment = VerticalAlignment.Top;
-
-            ColumnDefinition AddressColumn = new();
-            ColumnDefinition ResponseTimeColumn = new();
-            ColumnDefinition ResponseCodeColumn = new();
-            ColumnDefinition ActivityColumn = new();
-            NewGrid.ColumnDefinitions.Add(AddressColumn);
-            NewGrid.ColumnDefinitions.Add(ResponseTimeColumn);
-            NewGrid.ColumnDefinitions.Add(ResponseCodeColumn);
-            NewGrid.ColumnDefinitions.Add(ActivityColumn);
-
-            RowDefinition Row = new();
-            NewGrid.RowDefinitions.Add(Row);
-
-            Entry SiteAdress = new();
-            SiteAdress.Placeholder = "https://github.com";
-            SiteAdress.HeightRequest = NewGrid.Height - 10;
-            SiteAdress.WidthRequest = NewGrid.Width * 3 / 5;
-            Grid.SetRow(SiteAdress, 1);
-            Grid.SetColumn(SiteAdress, 1);
-            NewGrid.Children.Add(SiteAdress);
-
-
-            Label ReturnCode = new();
-            ReturnCode.Text = "-";
-            ReturnCode.TextColor = Color.DarkGray;
-            Grid.SetRow(SiteAdress, 1);
-            Grid.SetColumn(SiteAdress, 2);
-            NewGrid.Children.Add(ReturnCode);
-
-
-            Label ResponseTime = new();
-            ResponseTime.Text = "-";
-            ResponseTime.TextColor = Color.DarkGray;
-            Grid.SetRow(SiteAdress, 1);
-            Grid.SetColumn(SiteAdress, 3);
-            NewGrid.Children.Add(ResponseTime);
-
-            ActivityIndicator LoadingActivity = new();
-            LoadingActivity.IsRunning = false;
-            LoadingActivity.Color = Color.DarkGray;
-            Grid.SetRow(SiteAdress, 1);
-            Grid.SetColumn(SiteAdress, 4);
-            NewGrid.Children.Add(ResponseTime);
-
-            //SiteCollection.C.Add(NewGrid);
         }
 
 
